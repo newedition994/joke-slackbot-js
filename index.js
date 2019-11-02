8ae97e5ec4be8f065f4d56744c2e5f8b03562152
Breyanna Vass <breyannavass@gmail.com>
2019-11-01 23:20:27 -0400

    can now generate a joke from chuck norris

--- index.js
+++ index.js
@@ -32,3 +32,23 @@ bot.on("message", data => {
 
   handleMessage(data.text);
 });
+
+// Response to Data
+function handleMessage(message) {
+  if (message.includes(" chucknorris")) {
+    chuckJoke();
+  }
+}
+
+// Tell a Chuck Norris Joke
+function chuckJoke() {
+  axios.get("http://api.icndb.com/jokes/random/").then(res => {
+    const joke = res.data.value.joke;
+
+    const params = {
+      icon_emoji: ":laughing:"
+    };
+
+    bot.postMessageToChannel("general", `Chuck Norris: ${joke}`, params);
+  });
+}
